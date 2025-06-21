947d288b1d5d64bb7d833edafe192441b17f3f54
Diego1806-code <[diego.f.b.code@gmail.com]>
2025-06-21 16:32:36 +0200

    added project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -14,6 +14,20 @@ const Projects = () => {
               <span>TypeScript</span>
             </div>
           </div>
+          <div className="project-card">
+            <h3>Automation using Pyautogui</h3>
+            <p>
+              An automation using Pyautogui apllied in healthcare for advanced
+              privacy
+            </p>
+            <a href="https://pubmed.ncbi.nlm.nih.gov/40200470/">
+              view publication here
+            </a>
+            <br></br>
+            <a href="https://ebooks.iospress.nl/doi/10.3233/SHTI250073">
+              view paper here
+            </a>
+          </div>
           {/* Add more project cards */}
         </div>
       </div>
